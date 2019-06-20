83c6b5e8a344f7768fe7e414872243fb6809c46c
Alison Golden <alisong779@yahoo.com>
2019-06-20 14:39:43 +0000

    Done.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-end
+  
+end 
+  
+  config.secret_key_base = 3dd314c3109ff7c4047b551070fd030c7c4ef19175c7f51c871539406385c72387d6710b088e3112f601f0a1e9ef59635a6d8d584a2df41b2941ff05d76a53bf
+  
